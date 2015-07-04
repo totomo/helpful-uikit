140b6b4f886448ad4ece281543d951ef250d0cd3
Kenta Tokumoto <totomo_1217@yahoo.co.jp>
2015-07-04 21:22:43 +0900

    Add gitignore

---
+++ Source/UIButton+SushiKit.swift
@@ -0,0 +1,9 @@
+//
+//  UIButton+SushiKit.swift
+//  SushiKit
+//
+//  Created by Kenta Tokumoto on 2015/07/04.
+//  Copyright (c) 2015年 Kenta Tokumoto. All rights reserved.
+//
+
+import Foundation
