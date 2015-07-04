84e97881b40294ed24c0f4b018f3ea094b7617df
Kenta Tokumoto <totomo_1217@yahoo.co.jp>
2015-07-04 21:02:08 +0900

    Initial commit

---
+++ SushiKit/SushiKit.h
@@ -0,0 +1,19 @@
+//
+//  SushiKit.h
+//  SushiKit
+//
+//  Created by Kenta Tokumoto on 2015/07/04.
+//  Copyright (c) 2015年 Kenta Tokumoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SushiKit.
+FOUNDATION_EXPORT double SushiKitVersionNumber;
+
+//! Project version string for SushiKit.
+FOUNDATION_EXPORT const unsigned char SushiKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SushiKit/PublicHeader.h>
+
+
